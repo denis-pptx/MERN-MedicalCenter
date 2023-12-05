de61d73caffd056e21d7b8773fc09e28e8ff0d9b
Denis-PC <denis.pptx@gmail.com>
2023-12-05 22:16:08 +0300

    табуляция

--- MedicalCenter-API/models/order.js
+++ MedicalCenter-API/models/order.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  service: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Service',
-    required: [true, 'Service reference is required for the order'],
-  },
-  name: {
-    type: String,
-    required: [true, 'Name is required for the order'],
-    trim: true,
-    minlength: [3, 'Name must be at least 3 characters long'],
-    maxlength: [50, 'Name cannot exceed 50 characters'],
-  },
-  phone: {
-    type: String,
-    required: [true, 'Phone is required for the order'],
-    match: [/^\+375-\d{2}-\d{3}-\d{2}-\d{2}$/, 'Invalid phone number format. Use +375-XX-XXX-XX-XX.']
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    trim: true,
-    match: [/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/, 'Please enter a valid email address'],
-  }
+    service: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service',
+        required: [true, 'Service reference is required for the order'],
+    },
+    name: {
+        type: String,
+        required: [true, 'Name is required for the order'],
+        trim: true,
+        minlength: [3, 'Name must be at least 3 characters long'],
+        maxlength: [50, 'Name cannot exceed 50 characters'],
+    },
+    phone: {
+        type: String,
+        required: [true, 'Phone is required for the order'],
+        match: [/^\+375-\d{2}-\d{3}-\d{2}-\d{2}$/, 'Invalid phone number format. Use +375-XX-XXX-XX-XX.']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        match: [/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/, 'Please enter a valid email address'],
+    }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
--- MedicalCenter-API/models/user.js
+++ MedicalCenter-API/models/user.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  login: {
-    type: String,
-    required: [true, 'Username is required'],
-    unique: true,
-    trim: true,
-    minlength: [3, 'Username must be at least 3 characters long'],
-    maxlength: [30, 'Username cannot exceed 30 characters'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    trim: true,
-    match: [/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/, 'Please enter a valid email address'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [6, 'Password must be at least 6 characters long'],
-  }
+	login: {
+		type: String,
+		required: [true, 'Username is required'],
+		unique: true,
+		trim: true,
+		minlength: [3, 'Username must be at least 3 characters long'],
+		maxlength: [30, 'Username cannot exceed 30 characters'],
+	},
+	email: {
+		type: String,
+		required: [true, 'Email is required'],
+		unique: true,
+		trim: true,
+		match: [/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/, 'Please enter a valid email address'],
+	},
+	password: {
+		type: String,
+		required: [true, 'Password is required'],
+		minlength: [6, 'Password must be at least 6 characters long'],
+	}
 });
 
 module.exports = mongoose.model('User', userSchema);
