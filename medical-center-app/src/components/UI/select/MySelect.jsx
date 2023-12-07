dc253aed73ab09c03518b35fc3b18bd6567abf1e
Denis-PC <denis.pptx@gmail.com>
2023-12-07 21:13:18 +0300

    select category

---
+++ medical-center-app/src/components/UI/select/MySelect.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './MySelect.module.css';
+
+const MySelect = ({ options, defaultName, value, onChange }) => {
+    return (
+        <select
+            className={styles.select}
+            value={value}
+            onChange={event => onChange(event.target.value)}
+        >
+            <option
+                className={styles.option} 
+                disabled
+                value=""
+            >
+                {defaultName}
+            </option>
+
+            {options.map(option =>
+                <option
+                    className={styles.option}
+                    key={option.value}
+                    value={option.value}>
+
+                    {option.name}
+                </option>
+            )}
+        </select>
+    );
+};
+
+export default MySelect;
--- medical-center-app/src/pages/Services.jsx
+++ medical-center-app/src/pages/Services.jsx
@@ -1,21 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ServiceList from '../components/Lists/ServiceList';
-
+import MySelect from '../components/UI/select/MySelect';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/service')
             .then(response => setServices(response.data))
             .catch(error => console.error('Error fetching services:', error));
-    }, []); 
+
+        axios.get('http://localhost:5000/api/category')
+            .then(response => setCategories(response.data))
+            .catch(error => console.error('Error fetching categories:', error));
+    }, []);
+
+    const filteredServices = selectedCategory
+        ? services.filter(service => service.category._id === selectedCategory)
+        : services;
 
     return (
-        <div class="page">
+        <div className="page">
             <h1>Services Page</h1>
-            <ServiceList services={services} />
+
+            <MySelect
+                value={selectedCategory}
+                onChange={(category) => setSelectedCategory(category)}
+                defaultName="Категория"
+                options={[
+                    { name: 'Все', value: '' },
+                    ...categories.map(category => ({
+                        name: category.name,
+                        value: category._id
+                    }))
+                ]}
+            />
+
+
+            <ServiceList services={filteredServices} />
         </div>
     );
 };
