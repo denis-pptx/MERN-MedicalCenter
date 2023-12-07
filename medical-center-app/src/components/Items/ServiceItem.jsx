2cdb559a6af759948600dafa91da9cef69cf63b3
Denis-PC <denis.pptx@gmail.com>
2023-12-07 20:38:31 +0300

    services output

--- medical-center-app/src/components/Items/ServiceItem.jsx
+++ medical-center-app/src/components/Items/ServiceItem.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import MyButton from '../UI/button/MyButton';
-import './Styles.css';
+import styles from './Styles.module.css';
 
 const ServiceItem = (props) => {
     return (
-        <div className="item">
-            <div className="item__content">
-                <strong>{props.service.name}</strong>
-                <div>
-                    {props.service.description}
-                    <br></br>
-                    {props.service.cost}$
+        <div className={styles.card}>
+            <div className={styles.cardContent}>
+                <div className={styles.title}>
+                    {props.service.name}
+                </div>
+                
+                <div className={styles.description}>
+                    Описание: {props.service.description}
+                </div>
+                <div className={styles.category}>
+                    Категория: {props.service.category.name}
+                </div>
+                <div className={styles.cost}>
+                    Стоимость: {props.service.cost}$
                 </div>
             </div>
-            <div className="item__btns">
-                <MyButton>Открыть</MyButton>
-                <MyButton>Удалить</MyButton>
+            <div className={styles.buttons}>
+                <MyButton className={styles.button}>Открыть</MyButton>
+                <MyButton className={styles.button}>Удалить</MyButton>
             </div>
         </div>
     );
