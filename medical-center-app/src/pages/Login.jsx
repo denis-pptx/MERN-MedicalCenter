e4bd2b9a44033a88e0492c1f56074c2572c09cf3
Denis-PC <denis.pptx@gmail.com>
2023-12-08 03:59:47 +0300

    login page style

--- medical-center-app/src/pages/Login.jsx
+++ medical-center-app/src/pages/Login.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useState } from 'react';
 import $api from '../http/index';
 import AuthContext from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import MyInput from '../components/UI/Inputs/MyInput';
+import MyButton from '../components/UI/button/MyButton';
+import './Styles.css'
 
 const Login = () => {
     const { setIsAuth } = useContext(AuthContext);
@@ -19,23 +22,39 @@ const Login = () => {
             setIsAuth(true);
             navigate('/');
         } catch (error) {
-            alert(`SERVER: ${error.response.data.error}`)
+            alert(`SERVER: ${error.response.data.error}`);
             console.error('Authorization error:', error);
         }
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form>
-                <label>Login:</label>
-                <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} />
-                <br />
-                <label>Password:</label>
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <br />
-                <button onClick={handleLogin}>Login</button>
-            </form>
+        <div class="loginContainer">
+            <div class="loginForm">
+                <h2>Авторизация</h2>
+                <br></br>
+
+                <form>
+                    <div class="form-group">
+                        <h3>Логин</h3>
+                        <MyInput
+                            type="text"
+                            value={login}
+                            onChange={(e) => setLogin(e.target.value)}
+                        />
+                    </div>
+
+                    <div class="form-group">
+                        <h3>Пароль</h3>
+                        <MyInput
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    
+                    <MyButton onClick={handleLogin}>Войти</MyButton>
+                </form>
+            </div>
         </div>
     );
 };
